f516d32cb5add9da45c0368feb4b82bc6fd22c8e
ITKris-lab <tic.kym24@gmail.com>
2025-06-03 09:12:38 -0400

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,6 +1,6 @@
 from supabase import create_client, Client
 
 url = "https://pufgxmvvddqzxpjrqxpm.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."  # tu anon key completa
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1Zmd4bXZ2ZGRxenhwanJxeHBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NjkzODgsImV4cCI6MjA2MzM0NTM4OH0.dnbj810dalr1VZ0je_LsBKddbb3LetY828sc8mSbaNU"  # tu anon key completa
 
 supabase: Client = create_client(url, key)
